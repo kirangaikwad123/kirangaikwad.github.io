2112f892b7475d6e56ef515b35d3926cab5f7884
kirangaikwad123 <101328128+kirangaikwad123@users.noreply.github.com>
2022-03-10 16:22:21 +0530

    Create onlinechat.js

---
+++ onlinechat.js
@@ -0,0 +1,11 @@
+{
+  "name": "chat",
+    "veresion": "1.0.0",
+      "description": "chat application",
+        "main": "server.js",
+          "scripts":{
+            "test": "echo \"error:no testspecified\" && exit 1"
+          }
+  "author": "your name",
+    "license:""ISC"
+}
